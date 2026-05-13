690419559508cbe34ca1a5178b329caad9e5c9a9
ag7935-nayan <ag7935@gurukul.org>
2026-05-13 15:52:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  tanstackStart: {
-    server: {
-      entry: "server",
-    },
-  },
-
   vite: {
     server: {
       host: "0.0.0.0",
